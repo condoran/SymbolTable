7756fe4783a7a2ab15b77dd675bdf186b3465c22
Condor Andrei <44895249+condoran@users.noreply.github.com>
2021-10-15 08:31:06 -0700

    Update main.java

--- src/main.java
+++ src/main.java
@@ -4,9 +4,14 @@ public class main {
         SymbolTable s = new SymbolTable();
         System.out.println(s.add("abc"));
         System.out.println(s.add("cba"));
+        System.out.println(s.add("1"));
+        System.out.println(s.add("\"string\""));
         System.out.println(s.add("cba"));
+        System.out.println("\n");
         System.out.println(s.search("abc"));
         System.out.println(s.search("cba"));
+        System.out.println(s.search("1"));
+        System.out.println(s.search("\"string\""));
         System.out.println(s.search("cbaa"));
     }
 }
